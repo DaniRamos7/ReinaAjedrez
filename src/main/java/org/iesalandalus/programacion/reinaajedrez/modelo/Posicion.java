4afa22a40142cc5c0a535caf20e9c736097f3e19
Daniel <Daniel@PC-DANI-RAMOS>
2022-11-15 13:41:17 +0100

    la clase posicion esta terminada y ha pasado los test

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -1,7 +1,69 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import java.util.Objects;
+
 public class Posicion {
+	
 	private int fila;
 	private char columna;
+	
+	public int getFila() {
+		return fila;
+	}
+	private void setFila(int fila) {
+		if (fila > 8 ) {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		} else if (fila < 1) {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		}
+		this.fila = fila;
+	}
+	public char getColumna() {
+		return columna;
+	}
+	private void setColumna(char columna) {
+		if (columna > 'h' ) {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		} else if (columna < 'a') {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		}
+		this.columna = columna;
+	}
+	public Posicion(int fila, char columna) {
+		setFila(fila);
+		setColumna(columna);
+		
+	}
+	
+	public Posicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new NullPointerException("ERROR: No es posible copiar una posición nula.");
+		}
+		setFila(posicion.getFila());
+		setColumna(posicion.getColumna());
+		
+		
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
+	}
+	@Override
+	public String toString() {
+		return String.format("fila=%s, columna=%s", fila, columna);
+	}
+	
+	
 
 }
