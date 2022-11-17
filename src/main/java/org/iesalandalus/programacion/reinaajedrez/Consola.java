6e62eeb83dcc2293a67ce7d0bfcc85a351a24af1
Daniel Ramos Sánchez <daniramiyos@gmail.com>
2022-11-17 10:29:22 +0100

    He creado el metodo mostraMenu.

---
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -0,0 +1,13 @@
+package org.iesalandalus.programacion.reinaajedrez;
+
+import org.iesalandalus.programacion.reinaajedrez.modelo.Color;
+import org.iesalandalus.programacion.utilidades.Entrada;
+
+public final class Consola {
+
+	private Consola() {
+
+	}
+
+	
+}
