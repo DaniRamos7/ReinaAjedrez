7bb7ab7ffb6f9649a70cf91835e1db7b6e9d2908
Daniel Ramos Sánchez <daniramiyos@gmail.com>
2022-11-17 10:33:03 +0100

    He creado el método mostrarMenuDirecciones.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -18,5 +18,52 @@ public final class Consola {
 
 	}
 
+	public void elegirOpcionMenu() {
+		int opcion;
+		do {
+			System.out.print("Elija una opción valida: ");
+			opcion = Entrada.entero();
+		} while (opcion < 0 || opcion > 3);
+		switch (opcion) {
+		case 0:
+			System.out.println("Has elegido salir.");
+			break;
+		case 1:
+			System.out.println("Has elegido crear reina por defecto.");
+			break;
+		case 2:
+			System.out.println("Has elegido crear reina eligiendo color.");
+			break;
+		case 3:
+			System.out.println("Has elegido la opción mover.");
+			break;
+
+		}
+	}
+
+	public void elegirColor(Color color) {
+
+		int opcion;
+		do {
+			System.out.print("Elige un color: ");
+			System.out.println("1.- Blanco" + Color.BLANCO);
+			System.out.println("2.- Negro" + Color.NEGRO);
+			System.out.println("0.- Salir.");
+			opcion = Entrada.entero();
+		} while (color != Color.BLANCO || color != Color.NEGRO);
+		switch (opcion) {
+		case 0:
+			System.out.println("Has elegido salir.");
+			break;
+		case 1:
+			System.out.println("Has elegido el color blanco." + Color.BLANCO);
+			break;
+		case 2:
+			System.out.println("Has elegido el color negro." + Color.NEGRO);
+			break;
+
+		}
+
+	}
 
 }
